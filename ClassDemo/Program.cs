96d1b201c4517430f7ca6151e29cdbdde2ac8dd3
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-15 16:54:12 -0500

    staff class with properties and methods set

--- ClassDemo/Program.cs
+++ ClassDemo/Program.cs
@@ -1,7 +1,49 @@
-﻿using System;
+﻿//directives
+using System;
 
 namespace ClassDemo
 {
+    class Staff
+    {
+        //fields with private access modifiers
+        //encapsulation enables an object to hide data and behaviour from other classes that do not need to know about them.
+        private string nameOfStaff;
+        private const int hourlyRate = 30;
+        private int hWorked;
+
+        //property declaration with private set
+        public int HoursWorked
+        { get
+            {
+                return hWorked;
+            }
+         private set
+            {
+                if (value > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
+
+        //Declaring and implementing a Method 
+        public void PrintMessage()
+        {
+            Console.WriteLine("Calculating Pay...");
+        }
+
+        public int CalculatePay()
+        {
+            PrintMessage();
+            int staffPay;
+            staffPay = hWorked * hourlyRate;
+
+            if (hWorked > 0)
+                return staffPay;
+            else
+                return 0;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
